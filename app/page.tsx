5a58218f2912310676c259739cc067cfef1785a3
kenxo9000 <kenxo9000@gmail.com>
2026-03-11 12:32:29 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:40,fontFamily:"Arial"}}>
+      <h1>ToolsArena</h1>
+      <p>Free tools that stay useful.</p>
+    </div>
+  );
+}
